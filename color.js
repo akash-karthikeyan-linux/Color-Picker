8f97ffeb83b5e5662527a69c8a2cf2faf24e033c
Akash Karthikeyan <65849775+akash-karthikeyan-linux@users.noreply.github.com>
2024-06-16 18:15:01 +0530

    Add files via upload

---
+++ color.js
@@ -0,0 +1,27 @@
+function OnButton1()
+{
+    let color = "#e0e0e0";
+    document.getElementById("colorPickerContainer").style.backgroundColor="#e0e0e0";
+    document.getElementById("selectedColorHexCode").textContent=color
+}
+
+function OnButton2()
+{
+    let color = "#6fcf97";
+    document.getElementById("colorPickerContainer").style.backgroundColor="#6fcf97";
+    document.getElementById("selectedColorHexCode").textContent=color
+}
+
+function OnButton3()
+{
+    let color = "#56ccf2";
+    document.getElementById("colorPickerContainer").style.backgroundColor="#56ccf2";
+    document.getElementById("selectedColorHexCode").textContent=color
+}
+
+function OnButton4()
+{
+    let color = "#bb6bd9";
+    document.getElementById("colorPickerContainer").style.backgroundColor="#bb6bd9";
+    document.getElementById("selectedColorHexCode").textContent=color
+}
